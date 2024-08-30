eb6badbe520e0c5005bb8944a9dc2a8d1467c626
mgfallen <minegoodfeeling@gmail.com>
2024-08-30 11:08:39 +0300

    init commit

---
+++ src/main/java/t1/holding/auth_example/AuthExampleApplication.java
@@ -0,0 +1,13 @@
+package t1.holding.auth_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/t1/holding/auth_example/AuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package t1.holding.auth_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
